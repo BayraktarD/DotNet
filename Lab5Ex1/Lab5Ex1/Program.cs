a6602255bde7f31dedc8ea9abf2f268037be006b
Dorin Bayraktar <bayraktar.dorin@gmail.com>
2023-07-24 19:04:11 +0300

    comment conditii

--- Lab5Ex1/Lab5Ex1/Program.cs
+++ Lab5Ex1/Lab5Ex1/Program.cs
@@ -4,6 +4,33 @@ using System.Collections.Generic;
 
 namespace Lab5Ex1
 {
+
+//    Catalog
+            //Un catalog contine
+            // numele clasei
+            // lista elevilor.
+            // O functie care va returna un string continand
+            //numele clasei si al fiecarui elev in parte.
+            // O functie “GetPremiantul” care va determina
+            //elevul cu media cea mai mare.
+            //• Elevii vor avea
+            // nume, prenume
+            // o lista de note specifica fiecarui elev.
+            // O functie care va calcula media elevului
+            // O functie care va returna un string sub forma
+            // nume, prenume
+            // nota0 nota1 nota2…. Notam
+            //• In functia “Main”
+            // Initializati 3 elevi
+            // Initializati un catalog adaugandu-I elevii
+            // Acordati note elevilor
+            // Apelati metoda GetPremiantul a catalogului si
+            //afisati elevul cu media cea mai mare
+            // Apelati metoedele care returneaza descrierile
+            //obiectelor si afisati-le rezultatele.
+            //• Realiazti diagrama UML a clasei
+            //• Atentie la : constructori, modificatori de
+            //acces, clase.
     class Program
     {
         static void Main(string[] args)
